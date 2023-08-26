471f2fa7a65a2f455ed7b65cde2076d32d3f3ec1
mohammedabdulshaik <mohammedabdulshaik173@gmail.com>
2023-08-26 22:14:34 +0530

    updated key in backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "s3buckerforbackend"
-    key            = "terraform_locks"
+    key            = "s3buckerforbackend/terraform.tfstate"
     region         = "eu-north-1"
     encrypt        = true
     dynamodb_table = "terraform_locks"
